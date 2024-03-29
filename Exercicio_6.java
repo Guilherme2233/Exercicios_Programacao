f31cd96a4382e101c708d24618522616daa54f12
Guilherme_Souza <guilhermesouza2233@gmail.com>
2024-03-28 21:08:24 -0300

    modified:   Exercicio_6.java

--- Exercicio_6.java
+++ Exercicio_6.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
 public class Exercicio_6 {
-    
+    public static void main(String[] args) {
+        /* Exercicio 6: Crie um programa que faça a tabuada do numero que o usuario digitar */
+
+        try (Scanner Input = new Scanner(System.in)) {
+            System.out.println("Digite o numero da tabuada que deseja ver: ");
+            int Numero = Input.nextInt();
+
+            System.out.println("______________________________________");
+
+            for (int i = 0; i < 11; i++) {
+                System.out.println(Numero + " X " + i + " = " + Numero * i);
+            }
+
+            System.out.println("______________________________________");
+
+        }
+
+    }
+
 }
