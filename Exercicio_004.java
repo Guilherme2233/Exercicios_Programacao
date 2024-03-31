910eeb8e01eb25a94d0b8ea5c8e5d2b08f8eed6f
Guilherme_Souza <104692870+Guilherme2233@users.noreply.github.com>
2024-03-30 23:08:40 -0300

    Update Exercicio_004.java

--- Exercicio_004.java
+++ Exercicio_004.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Exercicio_004 {
-    /*EXERCICIO 4: faça um algoritmo que calcule a media de um aluno. caso a media do aluno seja menor que 5, diga que ele reprovou, caso for igual ou maior que 5 diga que ele esta aprovado e caso a média seja igual a 10 diga que ele foi aprovado e os de parabens */
+    /*EXERCICIO 4: faça um algoritmo que calcule a media de um aluno. caso a media do aluno seja menor que 5, diga que ele reprovou,
+    caso for igual ou maior que 5 diga que ele esta aprovado e caso a média seja igual a 10 diga que ele foi aprovado e os de parabens */
 
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
