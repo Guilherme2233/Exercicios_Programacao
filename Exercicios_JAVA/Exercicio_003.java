3d07d0cf0165ba89e22e1fec902519e857a2b962
Guilherme_Souza <guilhermesouza2233@gmail.com>
2024-03-31 15:06:17 -0300

    modified:   Exercicios_JAVA/Exercicio_003.java

--- Exercicios_JAVA/Exercicio_003.java
+++ Exercicios_JAVA/Exercicio_003.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Exercicio_003 {  
-    /*rio digite 0 como divisor, imprima no terminal: "Erro, não é possível dividir um número porEXERCICIO 3: Realize as 4 operações básicas: Adição, Subtração, Multiplicação e Divisão. 
-    No entanto, permita que o número seja fornecido pelo usuário. 
-    Caso o usuá 0".
+    /*EXERCICIO 3: Crie um açgoritmo que faça as 4 operações basicas: Adição, subtração, multiplicação e divisão. caso o usuario digite 0, apareca "erro! não e possivel dividir por 0"
 */
     
     public static void main(String[] args) {
